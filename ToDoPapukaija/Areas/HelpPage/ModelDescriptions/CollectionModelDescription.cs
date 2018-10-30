37447ac01fb7db284241b1e95a896f6236509e00
Heimolainen <jouni.nieminen@outlook.com>
2018-10-30 09:44:54 +0200

    Add project files.

---
+++ ToDoPapukaija/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ToDoPapukaija.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ToDoPapukaija/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ToDoPapukaija.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
